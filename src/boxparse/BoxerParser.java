2298811a0639a04c7eaebdb6799289538ac86d1d
TheAwesomeGe <eugenioribeiro13@gmail.com>
2015-05-01 22:56:00 +0100

    Started parsing the main DRS

--- src/boxparse/BoxerParser.java
+++ src/boxparse/BoxerParser.java
@@ -3,6 +3,7 @@ package boxparse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -17,13 +18,27 @@ import org.xml.sax.SAXException;
 
 import boxparse.exception.UnableToParseException;
 import boxparse.interpretation.Interpretation;
+import boxparse.interpretation.Token;
+import boxparse.interpretation.drs.DRS;
 
+/**
+ * BoxerParser reads and parses the XML output of Boxer, generating an interpretation in Java.
+ * 
+ * @author Eugenio Ribeiro
+ */
 public class BoxerParser {
 	
+	/**
+	 * Gets the document element of the XML file.
+	 * 
+	 * @param filename The name of the XML file.
+	 * @return The document element.
+	 * @throws UnableToParseException if the file could not be parsed.
+	 */
 	private Element getXMLDocument(String filename) throws UnableToParseException {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		try {
-			factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+			factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false); // Disables auto validation
 		} catch (ParserConfigurationException e) {
 			throw new UnableToParseException(e.getMessage());
 		}
@@ -45,19 +60,123 @@ public class BoxerParser {
 		return dom.getDocumentElement();
 	}
 	
-	private List<Node> selectElements(NodeList nodes) {
-		List<Node> elements = new ArrayList<Node>();
+	/**
+	 * Selects only the element nodes of a node list. 
+	 * 
+	 * @param nodes The node list.
+	 * @return A list with the element nodes.
+	 */
+	private List<Element> selectElements(NodeList nodes) {
+		List<Element> elements = new ArrayList<Element>();
 		
 		for(int i = 0; i < nodes.getLength(); i++) {
 			Node node = nodes.item(i);
 			if(node.getNodeType() == Node.ELEMENT_NODE) {
-				elements.add(node);
+				elements.add((Element) node);
 			}
 		}
 		
 		return elements;
 	}
+	
+	/**
+	 * Generates the token map based on the words and POS and named entity tags.
+	 * 
+	 * @param words The element containing the words.
+	 * @param pos The element containing the POS tags.
+	 * @param ne The element containing the named entity tags.
+	 * @return The token map.
+	 */
+	private HashMap<String, Token> generateTokens(Element words, Element pos, Element ne) {
+		HashMap<String, Token> tokens = new HashMap<String, Token>();
+		
+		NodeList wordElements = words.getElementsByTagName("word");
+		for(int i = 0; i < wordElements.getLength(); i++) {
+			Element word = (Element) wordElements.item(i);
+			
+			String id = word.getAttribute("xml:id");
+			
+			tokens.put(id, new Token(id, word.getTextContent()));
+		}
+		
+		NodeList posElements = pos.getElementsByTagName("postag");
+		for(int i = 0; i < posElements.getLength(); i++) {
+			Element postag = (Element) posElements.item(i);
+			
+			String id = postag.getAttribute("index");
+			
+			tokens.get(id).setPOSTag(postag.getTextContent());
+		}
+		
+		NodeList neElements = ne.getElementsByTagName("netag");
+		for(int i = 0; i < neElements.getLength(); i++) {
+			Element netag = (Element) neElements.item(i);
+			
+			String id = netag.getAttribute("index");
+			
+			tokens.get(id).setNETag(netag.getTextContent());
+		}
+		
+		return tokens;
+	}
+	
+	private DRS parseDRS(Element drs, HashMap<String, Token> tokens) throws UnableToParseException {
+		
+		switch(drs.getNodeName()) {
+		case "drs":
+			return parseBasicDRS(drs, tokens);
+		case "merge":
+			return parseMergeDRS(drs, tokens);
+		case "smerge":
+			return parseMergeDRS(drs, tokens);
+		case "alfa":
+			return parseAlfaDRS(drs, tokens);
+		default:
+			throw new UnableToParseException("Invalid DRS type"); // This never happens
+		}
+	}
+	
+	private DRS parseAlfaDRS(Element drs, HashMap<String, Token> tokens) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	private DRS parseMergeDRS(Element drs, HashMap<String, Token> tokens) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private DRS parseBasicDRS(Element drs, HashMap<String, Token> tokens) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Generates an interpretation based on the multiple XML sections.
+	 * 
+	 * @param words The element containing the words.
+	 * @param pos The element containing the POS tags.
+	 * @param ne The element containing the named entity tags.
+	 * @param drs The element containing the main DRS.
+	 * @return The interpretation.
+	 * @throws UnableToParseException if the main DRS could not be parsed.
+	 */
+	private Interpretation generateInterpretation(Element words, Element pos, Element ne, Element drs) throws UnableToParseException {
+		HashMap<String, Token> tokens = generateTokens(words, pos, ne);
+		
+		DRS mainDRS = parseDRS(drs, tokens);
+		
+		Interpretation interpretation = new Interpretation();
+		return interpretation;
+	}
+
+	/**
+	 * Parses a Boxer XML file and generates an interpretation in Java.
+	 * 
+	 * @param filename The name of the XML file.
+	 * @return The interpretation.
+	 * @throws UnableToParseException if the file could not be parsed.
+	 */
 	public Interpretation parse(String filename) throws UnableToParseException {
 		
 		NodeList xdrss = getXMLDocument(filename).getElementsByTagName("xdrs");
@@ -69,7 +188,7 @@ public class BoxerParser {
 		Node xdrs = xdrss.item(0); // TODO: Edit here for multiple XDRSs
 		
 
-		List<Node> mainChildren = selectElements(xdrs.getChildNodes());
+		List<Element> mainChildren = selectElements(xdrs.getChildNodes());
 		
 		if(mainChildren.size() != 4 || !mainChildren.get(0).getNodeName().equals("words")
 				|| !mainChildren.get(1).getNodeName().equals("postags")
@@ -78,20 +197,15 @@ public class BoxerParser {
 			throw new UnableToParseException("Invalid DRS definition");
 		}
 		
-		Node words = mainChildren.get(0);
-		Node pos = mainChildren.get(1);
-		Node ne = mainChildren.get(2);
-		Node drs = mainChildren.get(3);
+		Element words = mainChildren.get(0);
+		Element pos = mainChildren.get(1);
+		Element ne = mainChildren.get(2);
+		Element drs = mainChildren.get(3);
 		
-
-
-
-			
-
-		Interpretation interpretation = new Interpretation();
-		return interpretation;
+		return generateInterpretation(words, pos, ne, drs);
 	}
 	
+
 	public static void main(String[] args) throws UnableToParseException {
 		BoxerParser parser = new BoxerParser();
 		
--- src/boxparse/interpretation/Token.java
+++ src/boxparse/interpretation/Token.java
@@ -1,11 +1,40 @@
 package boxparse.interpretation;
 
+/**
+ * A token represents a segment of an utterance.
+ * 
+ * @author Eugenio Ribeiro
+ */
 public class Token {
-	private String id;
-	private String word;
+	
+	/**
+	 * The token's id.
+	 */
+	private final String id;
+	
+	/**
+	 * The word.
+	 */
+	private final String word;
+	
+	/**
+	 * The POS tag associated with the token.
+	 */
 	private String pos;
+	
+	/**
+	 * The Named Entity tag associated with the token if it refers to a named entity.
+	 */
 	private String ne;
 	
+	/**
+	 * Instantiates a new token.
+	 *
+	 * @param id the id
+	 * @param word the word
+	 * @param pos The POS tag
+	 * @param ne The 
+	 */
 	public Token(String id, String word, String pos, String ne) {
 		this.id = id;
 		this.word = word;
@@ -13,23 +42,100 @@ public class Token {
 		this.ne = ne;
 	}
 	
+	/**
+	 * Instantiates a new token.
+	 *
+	 * @param id the id
+	 * @param word the word
+	 * @param pos the pos
+	 */
 	public Token(String id, String word, String pos) {
 		this(id, word, pos, "");
 	}
 	
+	/**
+	 * Instantiates a new token.
+	 *
+	 * @param id the id
+	 * @param word the word
+	 */
+	public Token(String id, String word) {
+		this(id, word, "", "");
+	}
+	
+	/**
+	 * Gets the id.
+	 *
+	 * @return the id
+	 */
 	public String getID() {
 		return id;
 	}
 	
+	/**
+	 * Gets the word.
+	 *
+	 * @return the word
+	 */
 	public String getWord() {
 		return word;
 	}
 	
+	/**
+	 * Gets the POS tag.
+	 *
+	 * @return the POS tag
+	 */
 	public String getPOSTag() {
 		return pos;
 	}
 	
+	/**
+	 * Sets the POS tag.
+	 *
+	 * @param pos the new POS tag
+	 */
+	public void setPOSTag(String pos) {
+		this.pos = pos;
+	}
+	
+	/**
+	 * Gets the NE tag.
+	 *
+	 * @return the NE tag
+	 */
 	public String getNETag() {
 		return ne;
+	}
+	
+	/**
+	 * Sets the NE tag.
+	 *
+	 * @param ne the new NE tag
+	 */
+	public void setNETag(String ne) {
+		this.ne = ne;
+	}
+	
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		final String NEW_LINE = System.getProperty("line.separator");
+		
+		builder.append("Token " + id + ":" + NEW_LINE);
+		builder.append("Word: " + word);
+		
+		if(!pos.isEmpty()) {
+			builder.append(" - POS: " + pos);
+		}
+		
+		if(!ne.isEmpty()) {
+			builder.append(" - NE: " + ne);
+		}
+		
+		builder.append(NEW_LINE);
+		
+		return builder.toString();
 	}
 }
